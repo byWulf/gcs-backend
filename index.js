b0d652b51f703d484b8189d96d7184ea97896325
michael_wolf <michael.wolf@atraveo.de>
2017-03-05 16:56:49 +0100

    Simple version of a game lobby implemented

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict';
 
 const FrontendCommunicator = require('./lib/frontendCommunicator');
-let frontendCommunicator = new FrontendCommunicator(3701);
+let frontendCommunicator = new FrontendCommunicator({
+    tcp: 3701,
+    websocket: 3700
+});
 
-const GameCommunicator = require('./lib/frontendCommunicator');
-let gameCommunicator = new FrontendCommunicator(3701);
+const GameCommunicator = require('./lib/gameCommunicator');
+let gameCommunicator = new GameCommunicator(3702);
 
-const chatService = require('./lib/service/chat');
-chatService.registerEvents(frontendCommunicator.eventEmitter);
-
-const gameService = require('./lib/service/game');
+const gameService = require('./lib/service/game.service');
 gameService.registerFrontendEvents(frontendCommunicator.eventEmitter);
-gameService.registerGameEvents(gameCommunicator.eventEmitter);
 
-const newsService = require('./lib/service/news');
-newsService.registerEvents(frontendCommunicator.eventEmitter);
+const userService = require('./lib/service/user.service');
+userService.registerEvents(frontendCommunicator.eventEmitter);
+
+const matchService = require('./lib/service/match.service');
+matchService.registerFrontendEvents(frontendCommunicator.eventEmitter);
+matchService.registerGameEvents(gameCommunicator.eventEmitter);
+matchService.registerUserEvents(userService.eventEmitter);
